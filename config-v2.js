95b64678c0b2a04b7133c501f87f961f3f062e3a
SL-DINU-BOY <139786264+SL-DINU-BOY@users.noreply.github.com>
2026-01-20 22:31:59 +0530

    Update DATABASE token to a new GitHub token

--- config-v2.js
+++ config-v2.js
@@ -1,4 +1,4 @@
-const DATABASE = "Fr4kzVFjnNGsKNNtZ56PV3OK6HpKBl1M77Wr" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
+const DATABASE = "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" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
 const BOT_NUMBER = "+94781306128" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
 const SESSION_ID = "PRABATH-MD~ulD1vA0tOo7Dz2M" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
