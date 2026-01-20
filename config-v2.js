4c47b3ebb773d4d32514ea2b111df30724251fa8
SL-DINU-BOY <139786264+SL-DINU-BOY@users.noreply.github.com>
2026-01-20 22:27:17 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const DATABASE = "" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
-const BOT_NUMBER = "" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
-const SESSION_ID = "" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
+const DATABASE = "Fr4kzVFjnNGsKNNtZ56PV3OK6HpKBl1M77Wr" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
+const BOT_NUMBER = "+94781306128" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
+const SESSION_ID = "PRABATH-MD~ulD1vA0tOo7Dz2M" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
 
 // ඉහත ඒව "" මැදට එකතු කරන්න වෙනත් කිසිම දෙයක් වෙනස් නොකරන්න ❌. (Add "" in the middle of the above and ❌ don't change anything else.)
